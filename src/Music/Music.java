0d9c6648c0b106ba07fb531428279a9ad94d6873
Ali <ali123ranjbar456@gmail.com>
2025-04-18 21:36:18 +0330

    writing Music constructor and play method

--- src/Music/Music.java
+++ src/Music/Music.java
@@ -7,5 +7,22 @@ public class Music {
     public User singer;
     int numberofStreams;
     public static ArrayList<Music> allMusics = new ArrayList<Music>();
+    public void Music(String title , User singer){
+        for(Music music : allMusics){
+            if(music.title.equals(title) , music.singer.equals(singer)){
+                throw new InvalidOperationException("this Music already exists..");
+            }
+        }
+        this.singer = singer;
+        this.title = title;
+         allMusics.add(this);
+    }
+    public void play(){
+        System.out.println("song:" + title +"\nartist:" + singer.username + "is now playing");
+        this.numberofStreams++;
 
+    }
+    public void search(String title){
+
+    }
 }
