69160130b953b47719cdda04eba5ceac0da3c0f8
Ali <ali123ranjbar456@gmail.com>
2025-04-18 21:52:25 +0330

    creating Playlist class attributes

--- src/Playlist/Playlist.java
+++ src/Playlist/Playlist.java
@@ -1,4 +1,12 @@
 package Playlist;
 
+import Music.Music;
+
+import java.util.ArrayList;
+
 public class Playlist {
+    private String name;
+    private ArrayList<Music> playList = new ArrayList<Music>();
+    private User owner;
+
 }
