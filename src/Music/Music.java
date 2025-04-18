78dd537c7054f9222d7a8f8cf584708b0c7b5b02
Ali <ali123ranjbar456@gmail.com>
2025-04-18 21:28:32 +0330

    creating music class attributes

--- src/Music/Music.java
+++ src/Music/Music.java
@@ -1,4 +1,11 @@
 package Music;
 
+import java.util.ArrayList;
+
 public class Music {
+    public String title;
+    public User singer;
+    int numberofStreams;
+    public static ArrayList<Music> allMusics = new ArrayList<Music>();
+
 }
