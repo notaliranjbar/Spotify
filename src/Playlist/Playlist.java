81e2da6a00eaed65ff9c94101a560c02bba99dc3
Ali <ali123ranjbar456@gmail.com>
2025-04-18 21:55:42 +0330

    creating Playlist class methods

--- src/Playlist/Playlist.java
+++ src/Playlist/Playlist.java
@@ -5,8 +5,18 @@ import Music.Music;
 import java.util.ArrayList;
 
 public class Playlist {
-    private String name;
+    private String title;
     private ArrayList<Music> playList = new ArrayList<Music>();
     private User owner;
+    //methods
+    public void editTitle(String title){
+        this.title = title;
+    }
+    public void addMusic(Music music){
+
+    }
+    public void removeMusic(Music music){
+
+    }
 
 }
