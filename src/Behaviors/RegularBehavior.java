f6fe70ec70fd90eed42ecb5b29576e340320ba91
Ali <ali123ranjbar456@gmail.com>
2025-04-18 23:13:54 +0330

    adding playMusic method to regularbehavior class

--- src/Behaviors/RegularBehavior.java
+++ src/Behaviors/RegularBehavior.java
@@ -1,5 +1,8 @@
 package Behaviors;
 
+import Exceptions.InvalidOperationException;
+import Music.Music;
+
 public class RegularBehavior implements UserBehavior {
     private int playinglimit;
 
@@ -11,4 +14,19 @@ public class RegularBehavior implements UserBehavior {
     public int getPlayingLimit(){
         return this.playinglimit;
     }
+    //methods
+    @Override
+    public void creatPlaylist(String title , User owner){
+        throw new InvalidOperationException("you have to but premium to create a playlist");
+    }
+
+    @Override
+    public void playMusic(Music music){
+        if(music == null)throw new InvalidOperationException("choose a music to play");
+        if (this.playinglimit == 0) throw new InvalidOperationException("music playing limit is reached");
+        music.play();
+        this.playinglimit--;
+        System.out.println(music.getTitle() + "is playing");
+    }
+
 }
